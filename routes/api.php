e9e1a4d0ed5ce8829045c2b4e2147d4ab71921c0
VitorVts <vitorgguimaraes56@gmail.com>
2025-05-01 18:11:18 -0300

    Create Api Route to user

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/createUser', [UserController::class, 'createUser']);
-
-
+Route::get('/destroy/{id}', [UserController::class, 'destroyUser']);
+Route::get('user/{id}', [UserController::class, 'getUser']);
+Route::get('users', [UserController::class, 'getUsers']);
 
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,3 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/destroy/{id}', [UserController::class, 'destroyUser']);
-Route::get('user/{id}', [UserController::class, 'getUser']);
-Route::get('users', [UserController::class, 'getUsers']);
-
-Route::post('/createUser', [UserController::class, 'createUser']);
